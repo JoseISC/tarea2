2e6b71c57b5e31c0a60f18aafdbe226cb69071a0
Jose Salazar <j.salazarcabello@uandresbello.edu>
2025-04-30 13:50:23 +0000

    Pruebas de MainWindow

--- src/tarea2/frontend/MainWindow.java
+++ src/tarea2/frontend/MainWindow.java
@@ -1,15 +1,25 @@
 package tarea2.frontend;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainWindow extends JFrame{
+public class MainWindow extends JFrame implements ActionListener {
     private JPanel MainPanel;
     private JTextField textField1;
-    private JButton button1;
+    private JButton copiarButton;
+    private JLabel textoCopiado;
 
     public MainWindow(){
         this.setContentPane(this.MainPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.copiarButton.addActionListener(this);
         this.pack();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        String texto = this.textField1.getText();
+        this.textoCopiado.setText(texto);
     }
 }
